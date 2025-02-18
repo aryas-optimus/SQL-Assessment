28da95d5eb1cd9e966382753c0810a245301276d
aryas-optimus <aryapratap.singh@optimusinfo.com>
2025-02-18 13:56:51 +0530

    Add files via upload

---
+++ Assessment.sql
@@ -0,0 +1,107 @@
+CREATE TABLE t_atten_det(
+Atten_id INT IDENTITY(1001,1),
+Emp_id INT,
+FOREIGN KEY (Emp_id) REFERENCES t_emp(Emp_id), 
+Activity_id INT,
+FOREIGN KEY (Activity_id) REFERENCES t_activity(Activity_id),
+Atten_start_datetime DATETIME,
+Atten_end_hrs INT
+);
+INSERT INTO t_atten_det(Emp_id, Activity_id, Atten_start_datetime, Atten_end_hrs)
+VALUES
+(1001, 5, '2/13/2011 10:00:00', 2),
+(1001, 1, '1/14/2011 10:00:00', 3),
+(1001, 3, '1/14/2011 13:00:00', 5),
+(1003, 5, '2/16/2011 10:00:00', 8),
+(1003, 5, '2/17/2011 10:00:00', 8),
+(1003, 5, '2/19/2011 10:00:00', 7);
+
+SELECT * FROM t_atten_det
+
+CREATE TABLE t_salary(
+Salary_id INT,
+Emp_id INT,
+Changed_date DATE,
+New_Salary DECIMAL(10, 2)
+);
+
+INSERT INTO t_salary (salary_id, Emp_ID, Changed_date, New_Salary)
+VALUES
+(1001, 1003, '2/16/2011', 20000.00),
+(1002, 1003, '1/05/2011', 25000.00),
+(1003, 1001, '2/16/2011', 26000.00);
+
+
+DROP TABLE t_emp
+DROP TABLE t_salary
+DROP TABLE t_activity
+DROP TABLE t_atten_det
+
+CREATE TABLE t_emp(
+Emp_id INT IDENTITY(1001, 2) PRIMARY KEY NOT NULL,
+Emp_Code VARCHAR(255),
+Emp_f_name VARCHAR(255) NOT NULL,
+Emp_m_name VARCHAR(255),
+Emp_l_name VARCHAR(255),
+EMP_DOB DATE,-- INT CHECK DATEDIFF(Emp_DOB, GETDATE())>=18,
+EMP_DOJ DATE NOT NULL
+);
+
+
+
+INSERT INTO t_emp (Emp_Code, Emp_f_name, Emp_l_name, Emp_DOB, EMP_DOJ)
+VALUES
+('OPT20110105', 'Manmohan', 'Singh', '02/10/1983', '05/25/2010');
+
+INSERT INTO t_emp (Emp_Code, Emp_f_name, Emp_m_name, Emp_l_name, Emp_DOB, EMP_DOJ)
+VALUES
+('OPT20100915', 'Alfred', 'Joseph', 'Lawrence', '02/28/1988', '02/28/1988');
+
+CREATE TABLE t_activity(
+Activity_id INT PRIMARY KEY,
+Activity_description VARCHAR(255)
+);
+
+INSERT INTO t_activity(Activity_id, Activity_description)
+VALUES 
+(1, 'Code Analysis'),
+(2, 'Lunch'),
+(3, 'Coding'),
+(4, 'Knowledge Transition'),
+(5, 'Database');
+
+
+
+SELECT * FROM t_atten_det;
+SELECT * FROM t_salary;
+SELECT * FROM t_emp;
+SELECT * FROM t_activity;
+
+--RANK() OVER(PARTITION BY E.Emp_id ORDER BY S.New_Salary DESC) AS Rank
+
+
+
+--Q1
+
+SELECT CONCAT(Emp_f_name,' ', Emp_m_name,' ', Emp_l_name) AS Full_Name, E.EMP_DOB
+FROM t_emp E
+WHERE (DAY(Emp_DOB)=28 AND MONTH(Emp_DOB)=2) OR (DAY(Emp_DOB)=31 AND MONTH(Emp_DOB) IN (1,3,5,7,8,10,11)) OR (DAY(Emp_DOB)=30 AND MONTH(Emp_DOB) IN (4,6,9,11));
+
+
+--Q2.Display full name of employees who got increment in salary, previous salary, current salary, total worked hours, last worked activity and hours worked in that
+
+WITH SAL AS(
+SELECT E.Emp_id, ROW_NUMBER() OVER(PARTITION BY E.Emp_id ORDER BY S.Salary ASC) ROWN
+FROM t_emp E
+JOIN t_Salary S ON E.Emp_id=S.Emp_id),
+
+
+ACTIVITY AS(
+SELECT A.Emp_ID, E.Emp_f_name, E.Emp_m_name, E.Emp_l_name, S.New_Salary, ROW_NUMBER() OVER(PARTITION BY A.Emp_ID ORDER BY Atten_start_datetime DESC)
+FROM t_emp E
+JOIN t_attent_det Att ON E.Emp_iD=Att.Emp_id
+JOIN t_activity A ON E.Activity_id=A.Activity_id
+)
+
+
+
